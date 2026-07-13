ca2691cd4b10412282f66e82fd57dfb828114538
Donevin R. T. Z Frownfelter(OPERATOR-01) <bospaladin34@gmail.com>
2026-07-13 14:25:34 -0600

    Module Synthesis: Pulse 15070

--- generated_modules/module_15070.ts
+++ generated_modules/module_15070.ts
@@ -1,4 +1,4 @@
-// Generated at 2026-07-13T19:40:55.883Z
+// Generated at 2026-07-13T20:25:31.212Z
 export const pulse = 15070;
-export const pCore = 1.3710405122189475;
-export const turbulence = 0.06111;
+export const pCore = 1.0653937611429645;
+export const turbulence = 0.03922;
